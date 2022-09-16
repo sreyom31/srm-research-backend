a77fe672bc54028e0eb6ef7bef32340fa121a55e
sreyom31 <sreyom31@gmail.com>
2022-09-16 14:30:29 +0530

    date model changes
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/models/dates/date.schema.ts
+++ src/models/dates/date.schema.ts
@@ -1,25 +1,14 @@
 import { Schema } from 'mongoose';
 
 const DateSchema = new Schema({
-  paperSubmission: {
-    type: Date,
-    required: [true, 'Paper submission date is required'],
+  name: {
+    type: String,
+    required: [true, 'Name of the date field is required'],
     trim: true,
   },
-  notificationDeadline: {
+  date: {
     type: Date,
-    required: [true, 'Notification deadline date is required'],
-    trim: true,
-  },
-  cameraReadyDeadline: {
-    type: Date,
-    required: [true, 'Camera ready deadline date is required'],
-    trim: true,
-  },
-  revisedPaperSubmission: {
-    type: Date,
-    required: [true, 'Revised paper submission date is required'],
-    trim: true,
+    required: [true, 'Date is required'],
   },
 });
 
--- src/models/dates/date.types.ts
+++ src/models/dates/date.types.ts
@@ -1,8 +1,6 @@
 import { Document } from 'mongoose';
 
 export interface IDate extends Document {
-  paperSubmission: Date;
-  notificationDeadline: Date;
-  cameraReadyDeadline: Date;
-  revisedPaperSubmission: Date;
+  name: string;
+  date: Date;
 }
