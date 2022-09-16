7eff410bb220a2b5464ca06096342573324087df
sreyom31 <sreyom31@gmail.com>
2022-09-16 13:36:34 +0530

    user model
    Signed-off-by: sreyom31 <sreyom31@gmail.com>

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,18 +1,18 @@
-import { Router } from 'express';
+// import { Router } from 'express';
 
-const router = Router();
+// const router = Router();
 
-const defaultRoutes = [
-  { path: '/auth', route: authRoute },
-  { path: '/users', route: userRoute },
-  { path: '/papers', route: paperRoute },
-  { path: '/speakers', route: speakerRoute },
-  { path: '/dates', route: dateRoute },
-  { path: '/gallery', route: galleryRoute },
-];
+// const defaultRoutes = [
+//   { path: '/auth', route: authRoute },
+//   { path: '/users', route: userRoute },
+//   { path: '/papers', route: paperRoute },
+//   { path: '/speakers', route: speakerRoute },
+//   { path: '/dates', route: dateRoute },
+//   { path: '/gallery', route: galleryRoute },
+// ];
 
-defaultRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-}
+// defaultRoutes.forEach((route) => {
+//     router.use(route.path, route.route);
+// }
 
-export default router;
+// export default router;
